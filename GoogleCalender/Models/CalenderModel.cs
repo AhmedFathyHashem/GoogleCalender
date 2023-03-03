92b695c3e8b1d49333b0f42b297f1c465e512899
whitedevil1993 <ahmedfathy@msn.cn>
2023-03-03 19:05:33 +0200

    Add project files.

---
+++ GoogleCalender/Models/CalenderModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GoogleCalender.Models
+{
+    public class CalenderModel
+    {
+        public string Id { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public string Start { get; set; }
+        public string End { get; set; }
+    }
+}
